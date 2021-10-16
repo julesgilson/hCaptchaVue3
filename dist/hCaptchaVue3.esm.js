5c774fcc5a4d3a151903cb1b29044fb6049fd196
Jules Gilson (Hades) <julesgilson@yahoo.co.uk>
2021-10-16 09:55:56 +0100

    version 0.1.0 build

--- dist/hCaptchaVue3.esm.js
+++ dist/hCaptchaVue3.esm.js
@@ -6,14 +6,25 @@ var script = {
         size: { default: 'normal', type: String },
         theme: { default: 'light', type: String },
         tabindex: { default: '0', type: String },
-        apiUrl: { default: 'https://js.hcaptcha.com/1/api.js?render=onload&recaptchacompat=off', type: String },
-        veryifyEndpoint: { default: '', type: String },
-        inclCredentials: { default: false, type: Boolean },
+        apiurl: { default: 'https://js.hcaptcha.com/1/api.js', type: String },
+        validateendpoint: { default: '', type: String },
+        inclcredentials: { default: false, type: Boolean },
     },
-    emits: ['error', 'verified', 'expired', 'challengeExpired', 'opened', 'closed', 'reset', 'rendered', 'executed', 'serverResponse'],
+    emits: ['error', 'verified', 'expired', 'challengeExpired', 'opened', 'closed', 'serverResponse'],
     setup: function setup(props, context) {
         var theProps = toRefs(props);
         var hcaptchaInstanceId = null;
+        var key;
+        var eKey;
+
+        var apiAddress = theProps.apiurl.value;
+        var urlParams = {
+            render: 'explicit',
+            recaptchacompat: 'off',
+        };
+        Object.keys(urlParams).forEach(function (key, index) {
+            apiAddress = index === 0 ? (apiAddress + "?" + key + "=" + (urlParams[key])) : (apiAddress + "&" + key + "=" + (urlParams[key]));
+        });
 
         function apiLoadError(event) {
             document.querySelector('#hCaptchaVue3Comp p').style = 'display: block';
@@ -42,37 +53,41 @@ var script = {
             }
         }
 
-        function respexpired(resp) {
-            context.emit('expired', resp);
+        function respexpired() {
+            context.emit('expired', 'expired');
         }
-        function respchalexpired(resp) {
-            context.emit('challengeexpired', resp);
+        function respchalexpired() {
+            context.emit('challengeexpired', 'chalexpired');
         }
-        function respopened(resp) {
-            context.emit('opened', resp);
+        function respopened() {
+            context.emit('opened', 'opened');
         }
-        function respclosed(resp) {
-            context.emit('closed', resp);
+        function respclosed() {
+            context.emit('closed', 'closed');
         }
-        function resperror(resp) {
-            context.emit('error', resp);
+        function resperror() {
+            context.emit('error', 'error');
         }
 
         function captchaVerified() {
-            var key = window.hcaptcha.getResponse(hcaptchaInstanceId);
-            var eKey = window.hcaptcha.getRespKey(hcaptchaInstanceId);
-            if (!theProps.veryifyEndpoint.value.length) { return context.emit('verified', { key: key, eKey: eKey }); }
+            key = window.hcaptcha.getResponse(hcaptchaInstanceId);
+            eKey = window.hcaptcha.getRespKey(hcaptchaInstanceId);
+            context.emit('verified', { key: key, eKey: eKey });
+            if (theProps.validateendpoint.value.length) {
+                serverConfirm();
+            }
+        }
+
+        function serverConfirm() {
             //run fetch
-            var frmData = new FormData();
+            var frmData = buildFormData();
             var rqstParams = {
                 method: 'post',
                 body: frmData,
                 cache: 'no-cache',
             };
-            if (theProps.inclCredentials.value) { rqstParams.credentials = 'include'; }
-            frmData.append('key', key);
-            frmData.append('eKey', eKey);
-            fetch(theProps.veryifyEndpoint.value, rqstParams)
+            if (theProps.inclcredentials.value) { rqstParams.credentials = 'include'; }
+            fetch(theProps.validateendpoint.value, rqstParams)
                 .then(function (response) {
                     context.emit('serverResponse', {
                         status: response.status,
@@ -87,12 +102,24 @@ var script = {
                 });
         }
 
+        function buildFormData() {
+            var frmData = new FormData();
+            frmData.append('key', key);
+            frmData.append('eKey', eKey);
+            context.slots.default().forEach(function (slot) {
+                if (typeof slot == 'object' && slot.type == 'input' && slot.props.type == 'hidden' && slot.props.name.length) {
+                    frmData.append(slot.props.name, slot.props.value);
+                }
+            });
+            return frmData;
+        }
+
         var scriptEl = h('script', {
             async: true,
             defer: true,
             onLoad: function () { return apiLoaded(); },
             onError: function (event) { return apiLoadError(event); },
-            src: theProps.apiUrl.value,
+            src: apiAddress,
         });
         var errorEl = h(
             'p',
